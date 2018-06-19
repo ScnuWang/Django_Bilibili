09442eddd9d67fc6576ab1da12244c8b3da2377a
ScnuWang <scnu_wang@163.com>
2018-06-19 17:37:15 +0800

    装饰后台管理

--- mysite/article/admin.py
+++ mysite/article/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Article
 # Register your models here.
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    # 设置admin平台显示模式的内容
+    list_display = ('id','tittle','context')
+    # 排序：倒序在id 前面加负号
+    ordering = ('id',)
+
+# 可通过装饰器的方式代替：主要是为了防止要注册的类太多了不方便看
+# admin.site.register(Article,ArticleAdmin)
--- mysite/article/models.py
+++ mysite/article/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Article(models.Model):
     tittle = models.CharField(max_length=30)
     context = models.TextField()
+
+    #
+    def __str__(self):
+        return "<Article: %s>" % self.tittle
