d6224b7767d99fdcec44901736b969149a7f640b
ScnuWang <scnu_wang@163.com>
2018-06-20 10:15:47 +0800

    修改时区，后台对象显示中文标题

--- mysite/article/models.py
+++ mysite/article/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 # 修改模型之后需要重新执行迁移数据库，再执行之前，先备份数据库
 class Article(models.Model):
-    tittle = models.CharField(max_length=30)
-    context = models.TextField()
+    tittle = models.CharField(max_length=30,verbose_name="标题")
+    context = models.TextField(verbose_name="内容")
     # 注意二者的区别
-    created_time = models.DateTimeField(auto_now_add=True) # 在创建数据迁移文件的时候不能自动添加时间?
-    last_update_time = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User,on_delete=models.DO_NOTHING,default=1)
-    is_delete = models.BooleanField(default=False)
-    read_num = models.IntegerField(default=0)
+    created_time = models.DateTimeField(auto_now_add=True,verbose_name="创建时间") # 在创建数据迁移文件的时候不能自动添加时间?
+    last_update_time = models.DateTimeField(auto_now=True,verbose_name="上次修改时间")
+    author = models.ForeignKey(User,on_delete=models.DO_NOTHING,default=1,verbose_name="作者")
+    is_delete = models.BooleanField(default=False,verbose_name="是否删除")
+    read_num = models.IntegerField(default=0,verbose_name="阅读次数")
 
 
 
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'zh-Hans' # 中文繁体 ： zh-Hant
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
