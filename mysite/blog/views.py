7e43fbbe998b62d0f3dd5782c057dd79ca87812f
ScnuWang <scnu_wang@163.com>
2018-06-21 15:37:21 +0800

    添加分页组件

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -20,10 +20,16 @@ def blog_detail(request,blog_pk):
     return render_to_response("blog/blog_detail.html",context)
 
 def blog_with_type(request,blog_type_pk):
-    context = {}
+
     blogtype = get_object_or_404(BlogType,pk=blog_type_pk)
-    # 获取分类名称
+    blog_all_list = Blog.objects.filter(blogtype=blogtype)
+
+    paginator = Paginator(blog_all_list, 10)  # 每页10篇
+    page_num = request.GET.get('page', 1)  # 获取Get请求参数
+    page_of_blog = paginator.get_page(page_num)
+
+    context = {}
     context['blogtype'] = blogtype
-    context['blogs'] = Blog.objects.filter(blogtype=blogtype)
+    context['page_of_blog'] = page_of_blog
     context['blogtypes'] = BlogType.objects.all()
     return  render_to_response("blog/blog_with_type.html",context)
