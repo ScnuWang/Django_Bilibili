134ecfe9ed858ea416410fc69c21f9579293463f
ScnuWang <scnu_wang@163.com>
2018-06-22 14:59:56 +0800

    优化重复代码

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,26 +5,26 @@ from django.core.paginator import Paginator
 # 也可以在settings.py文件里面配置
 each_page_blogs_num = 2
 
-# Create your views here.
-def blog_list(request):
-    blog_all_list = Blog.objects.all();
-    paginator = Paginator(blog_all_list,each_page_blogs_num)# 每页10篇
-    page_num = request.GET.get('page',1)# 获取Get请求参数
-    page_of_blog = paginator.get_page(page_num)# 当前分页对象
+# 提取公共的代码
+def blog_list_common_util(request,blog_list):
+
+    paginator = Paginator(blog_list, each_page_blogs_num)  # 每页10篇
+    page_num = request.GET.get('page', 1)  # 获取Get请求参数
+    page_of_blog = paginator.get_page(page_num)  # 当前分页对象
     current_page_num = page_of_blog.number
     # 获取当前页面前后各两页的范围
-    page_range = list(range(max(current_page_num-2,1),current_page_num))+ list(range(current_page_num,min(current_page_num+2,paginator.num_pages)+1))
+    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + list(range(current_page_num, min(current_page_num + 2, paginator.num_pages) + 1))
 
     # 添加省略页码标记
     if page_range[0] - 1 >= 2:
-        page_range.insert(0,'...')
+        page_range.insert(0, '...')
     if paginator.num_pages - page_range[-1] >= 2:
         page_range.append('...')
 
     # 添加第一页和最后一页
-    if page_range[0] != 1 :
-        page_range.insert(0,1)
-    if page_range[-1] != paginator.num_pages :
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
 
     # 按照时间排序
@@ -35,6 +35,15 @@ def blog_list(request):
     context['page_of_blog'] = page_of_blog
     context['blogtypes'] = BlogType.objects.all()
     context['blogs_with_date'] = blogs_with_date
+
+    return context
+
+
+
+# Create your views here.
+def blog_list(request):
+    blog_all_list = Blog.objects.all();
+    context = blog_list_common_util(request,blog_all_list)
     return render_to_response("blog/blog_list.html",context)
 
 
@@ -53,76 +62,20 @@ def blog_detail(request,blog_pk):
     context['next_blog'] = next_blog
     context['blog'] = get_object_or_404(Blog,pk=blog_pk)
     return render_to_response("blog/blog_detail.html",context)
+
+
 # 根据类型获取博客列表
 def blog_with_type(request,blog_type_pk):
 
     blogtype = get_object_or_404(BlogType,pk=blog_type_pk)
     blog_all_list = Blog.objects.filter(blogtype=blogtype)
-
-    paginator = Paginator(blog_all_list, each_page_blogs_num)  # 每页10篇
-    page_num = request.GET.get('page', 1)  # 获取Get请求参数
-    page_of_blog = paginator.get_page(page_num)
-
-    current_page_num = page_of_blog.number
-    # 获取当前页面前后各两页的范围
-    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + list(
-        range(current_page_num, min(current_page_num + 2, paginator.num_pages) + 1))
-
-    # 添加省略页码标记
-    if page_range[0] - 1 >= 2:
-        page_range.insert(0, '...')
-    if paginator.num_pages - page_range[-1] >= 2:
-        page_range.append('...')
-
-    # 添加第一页和最后一页
-    if page_range[0] != 1:
-        page_range.insert(0, 1)
-    if page_range[-1] != paginator.num_pages:
-        page_range.append(paginator.num_pages)
-
-    # 按照时间排序
-    blogs_with_date = Blog.objects.dates('created_time', 'month', order='DESC')
-
-    context = {}
-    context['page_range'] = page_range
+    context = blog_list_common_util(request,blog_all_list)
     context['blogtype'] = blogtype
-    context['page_of_blog'] = page_of_blog
-    context['blogtypes'] = BlogType.objects.all()
-    context['blogs_with_date'] = blogs_with_date
     return  render_to_response("blog/blog_with_type.html",context)
 
 # 根据时间获取博客列表
 def blog_with_date(request,year,month):
-    blogs_with_date = Blog.objects.filter(created_time__year=year,created_time__month=month)
-
-    paginator = Paginator(blogs_with_date, each_page_blogs_num)  # 每页10篇
-    page_num = request.GET.get('page', 1)  # 获取Get请求参数
-    page_of_blog = paginator.get_page(page_num)
-
-    current_page_num = page_of_blog.number
-    # 获取当前页面前后各两页的范围
-    page_range = list(range(max(current_page_num - 2, 1), current_page_num)) + list(
-        range(current_page_num, min(current_page_num + 2, paginator.num_pages) + 1))
-
-    # 添加省略页码标记
-    if page_range[0] - 1 >= 2:
-        page_range.insert(0, '...')
-    if paginator.num_pages - page_range[-1] >= 2:
-        page_range.append('...')
-
-    # 添加第一页和最后一页
-    if page_range[0] != 1:
-        page_range.insert(0, 1)
-    if page_range[-1] != paginator.num_pages:
-        page_range.append(paginator.num_pages)
-
-    # 按照时间排序
-    blogs_with_date = Blog.objects.dates('created_time', 'month', order='DESC')
-
-    context = {}
+    blog_list_with_date = Blog.objects.filter(created_time__year=year,created_time__month=month)
+    context = blog_list_common_util(request, blog_list_with_date)
     context['blog_with_date'] = "%s年 %s月" % (year,month)
-    context['page_range'] = page_range
-    context['page_of_blog'] = page_of_blog
-    context['blogtypes'] = BlogType.objects.all()
-    context['blogs_with_date'] = blogs_with_date
     return render_to_response("blog/blog_with_date.html", context)
